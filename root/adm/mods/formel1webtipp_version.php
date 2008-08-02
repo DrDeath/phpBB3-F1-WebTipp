6d1c6a6517454dc2e6887d4091f7ae8dc46257ad
Dr.Death <dr.death@lpi-clan.de>
2008-08-02 15:53:06 +0000

    added feature MOD Version check by handyman'

---
+++ root/adm/mods/formel1webtipp_version.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* @package acp
+* @version $Id$
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @package formel1webtipp
+*/
+class formel1webtipp_version
+{
+	function version()
+	{
+		return array(
+			'author'	=> 'Dr.Death',
+			'title'		=> 'Formel1WebTipp',
+			'tag'		=> 'formel1webtipp',
+			'version'	=> '0.1.30',
+			'file'		=> array('www.lpi-clan.de', 'updatecheck', 'formel1webtipp.xml'),
+		);
+	}
+}
+
+?>
