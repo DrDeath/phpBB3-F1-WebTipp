77a10db41340cde77814610025be81607688a19a
Dr.Death <dr.death@lpi-clan.de>
2009-03-08 09:30:41 +0000

    preparation for upcoming version 0.1.31

--- root/adm/mods/formel1webtipp_version.php
+++ root/adm/mods/formel1webtipp_version.php
@@ -19,7 +19,7 @@ class formel1webtipp_version
 			'author'	=> 'Dr.Death',
 			'title'		=> 'Formel1WebTipp',
 			'tag'		=> 'formel1webtipp',
-			'version'	=> '0.1.30',
+			'version'	=> '0.1.31',
 			'file'		=> array('www.lpi-clan.de', 'updatecheck', 'formel1webtipp.xml'),
 		);
 	}
