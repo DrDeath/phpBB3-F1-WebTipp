020358bce48932a443f85deb83d1ff38ca5ae89c
Dr.Death <dr.death@lpi-clan.de>
2009-12-12 20:14:18 +0000

    First commit with my new iMac ;-) Preparing new point calculating according to the new F1 Rules 2010

--- root/adm/mods/formel1webtipp_version.php
+++ root/adm/mods/formel1webtipp_version.php
@@ -19,7 +19,7 @@ class formel1webtipp_version
 			'author'	=> 'Dr.Death',
 			'title'		=> 'Formel1WebTipp',
 			'tag'		=> 'formel1webtipp',
-			'version'	=> '0.3.3',
+			'version'	=> '0.3.4',
 			'file'		=> array('www.lpi-clan.de', 'updatecheck', 'formel1webtipp.xml'),
 		);
 	}
