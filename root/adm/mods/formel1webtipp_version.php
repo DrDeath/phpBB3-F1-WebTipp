7fe876692a40ef1a83bf3e17db7b8f1b764febf9
Dr.Death <dr.death@lpi-clan.de>
2014-02-26 22:22:45 +0100

    Missed update to correct version number 1.2014.0

--- root/adm/mods/formel1webtipp_version.php
+++ root/adm/mods/formel1webtipp_version.php
@@ -23,7 +23,7 @@ class formel1webtipp_version
 			'author'	=> 'Dr.Death',
 			'title'		=> 'Formel1WebTipp',
 			'tag'		=> 'formel1webtipp',
-			'version'	=> '1.2013.2',
+			'version'	=> '1.2014.0',
 			'file'		=> array('www.lpi-clan.de', 'updatecheck', 'formel1webtipp.xml'),
 		);
 	}
